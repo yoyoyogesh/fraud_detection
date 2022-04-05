d678245cf4f384899ab1d9b3dbab4ffd997b344f
YogeshKolhe11 <yogeshkolhe11@gmail.com>
2022-04-05 21:47:39 +0530

    added 3 model in code

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,13 @@
+
 print("Adding all ML related code here")
+
+def model1():
+    print("Adding code for ML model1")
+
+
+def model2():
+    print("Adding code for ML model2")
+
+
+def model3():
+    print("Adding code for ML model3")
